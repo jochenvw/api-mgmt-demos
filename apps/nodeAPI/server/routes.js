dd9b47b36eb68247b01c0c83b39cce5cbead2868
Jochen van Wylick <jowylick@microsoft.com>
2021-10-12 11:14:23 +0000

    Added HTTP 200 @ / for health probles

--- apps/nodeAPI/server/routes.js
+++ apps/nodeAPI/server/routes.js
@@ -1,5 +1,8 @@
 import examplesRouter from './api/controllers/examples/router';
 
 export default function routes(app) {
+  app.get('/', function(req, res, next) {
+    res.status(200).end();
+  })
   app.use('/api/v1/examples', examplesRouter);
 }
