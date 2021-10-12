143e12709618508a60515f2119ed844f953c5f80
Jochen van Wylick <jowylick@microsoft.com>
2021-10-12 11:32:10 +0000

    Trying to get static file serving to work

--- apps/nodeAPI/server/routes.js
+++ apps/nodeAPI/server/routes.js
@@ -1,10 +1,11 @@
 import examplesRouter from './api/controllers/examples/router';
 import express from 'express';
+var path = require('path');
 
 export default function routes(app) {
   app.get('/', function(req, res, next) {
     res.status(200).end();
-  })
-  app.use('/spec', express.static('/common'))
+  });
+  app.use('/spec', express.static(path.join(__dirname, 'common')));
   app.use('/api/v1/examples', examplesRouter);
 }
