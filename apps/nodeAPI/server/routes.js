3acdccd064bc0e9de5a250cc08d1010583085b2c
Jochen van Wylick <jowylick@microsoft.com>
2021-10-12 11:20:57 +0000

    Added static file serving to serve openapi spec file

--- apps/nodeAPI/server/routes.js
+++ apps/nodeAPI/server/routes.js
@@ -1,8 +1,10 @@
 import examplesRouter from './api/controllers/examples/router';
+import express from 'express';
 
 export default function routes(app) {
   app.get('/', function(req, res, next) {
     res.status(200).end();
   })
+  app.use('/spec', express.static('/common'))
   app.use('/api/v1/examples', examplesRouter);
 }
